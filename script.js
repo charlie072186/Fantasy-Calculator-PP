db7b2d46730c2d026bcfde7a45e0e1310f6f3897
charlie072186 <michael30mch@gmail.com>
2025-05-16 19:57:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ function calculateScore() {
       total += diff;
     }
 
-    const finishingPoints = [45, 42, 41, 40, 39, 38, 37, 36, 35, 34, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    const finishingPoints = [45, 42, 41, 40, 39, 38, 37, 36, 35, 34, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,0];
     if (!isNaN(finish) && finish >= 1 && finish <= 40) {
       const pts = finishingPoints[finish - 1];
       breakdown += `Finishing Position (${finish}): ${pts} pts\n`;
