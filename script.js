e88cf8f0f56e9307e19251ffbc9805748244feb9
charlie072186 <michael30mch@gmail.com>
2025-05-27 15:20:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -374,4 +374,11 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+function copyExtraBreakdown() {
+  const box = document.getElementById("extra-breakdown-box");
+  box.select();
+  document.execCommand("copy");
+}
+
+
 window.onload = loadLeagues;
